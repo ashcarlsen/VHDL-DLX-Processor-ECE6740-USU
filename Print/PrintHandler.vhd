d3ef0b14049a901190a3673e5f89364a184e4021
Ashton Carlsen <ashtoncarlsen@yahoo.com>
2026-03-18 21:32:29 -0600

    Working DLX print. Got rid of inferred latches.

--- Print/PrintHandler.vhd
+++ Print/PrintHandler.vhd
@@ -20,35 +20,35 @@ end entity PrintHandler;
 
 architecture behavior of PrintHandler is
     -- state tracking signals
-    type state_type is (IDLE, REQUEST_FROM_PRINT, GET_FROM_PRINT, CHECK_TYPE, CHAR, INT, ONE, START, TWO, THREE, FOUR, FIVE, ADD_SIGN, POP, WR);
+    type state_type is (IDLE, REQUEST_FROM_PRINT, GET_FROM_PRINT, CHECK_TYPE, CHAR, INT, ONE, START, TWO, THREE, FOUR, ADD_STACK, ADD_SIGN, POP, LOAD, WR);
     signal state : state_type := IDLE;
     signal next_state : state_type := IDLE;
 
     -- stack stuff
     signal stack_wr : STD_LOGIC;
-    signal stack_val : STD_LOGIC_VECTOR(7 downto 0);
-    signal stack_addr : STD_LOGIC_VECTOR(3 downto 0);
+    signal stack_val : STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
+    signal stack_addr : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
+    signal next_addr : STD_LOGIC_VECTOR(3 downto 0) := (others => '0');
     signal stack_out : STD_LOGIC_VECTOR(7 downto 0);
     
     -- divider stuff
-    signal numer : STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
+    signal numer : STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0) := (others => '0');
+    signal next_numer : STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0) := (others => '0');
     signal quot : STD_LOGIC_VECTOR(DATA_WIDTH-1 downto 0);
     signal remain : STD_LOGIC_VECTOR(7 downto 0);
 begin
 
-    state_update : process(RST, DATA, EMPTY, FULL, state)
-    variable idx : integer := 0;
+    state_update : process(RST, DATA, EMPTY, FULL, state, numer, stack_addr, quot, remain, stack_out)
     begin
         stack_wr <= '0';
         stack_val <= (others => '0');
-        stack_addr <= STD_LOGIC_VECTOR(to_unsigned(idx, stack_addr'length));
-        numer <= (others => '0');
         case state is
             when IDLE =>
                 RD_REQ <= '0';
                 WR_EN <= '0';
                 CHAR_OUT <= (others => '0');
-                idx := 0;
+                next_addr <= (others => '0');
+                next_numer <= (others => '0');
                 if (EMPTY = '1') or (FULL = '1') then
                     next_state <= IDLE;
                 else
@@ -58,11 +58,15 @@ begin
                 CHAR_OUT <= (others => '0');
                 WR_EN <= '0';
                 RD_REQ <= '1';
+                next_addr <= stack_addr;
+                next_numer <= numer;
                 next_state <= GET_FROM_PRINT;
             when GET_FROM_PRINT =>
                 CHAR_OUT <= (others => '0');
                 RD_REQ <= '0';
                 WR_EN <= '0';
+                next_addr <= stack_addr;
+                next_numer <= numer;
                 next_state <= CHECK_TYPE;
             when CHECK_TYPE =>
                 if (DATA(33) = '1') then
@@ -73,15 +77,21 @@ begin
                 CHAR_OUT <= (others => '0');
                 WR_EN <= '0';
                 RD_REQ <= '0';
+                next_addr <= stack_addr;
+                next_numer <= numer;
             when CHAR =>
                 CHAR_OUT <= DATA(7 downto 0);
                 WR_EN <= '1';
                 RD_REQ <= '0';
+                next_addr <= stack_addr;
+                next_numer <= numer;
                 next_state <= IDLE;
             when INT => 
                 CHAR_OUT <= (others => '0');
                 WR_EN <= '0';
                 RD_REQ <= '0';
+                next_addr <= stack_addr;
+                next_numer <= numer;
                 next_state <= START;
             when START =>
                 CHAR_OUT <= (others => '0');
@@ -89,44 +99,53 @@ begin
                 RD_REQ <= '0';
                 -- If signed value
                 if DATA(32) = '1' and DATA(31) = '1' then
-                    numer <= STD_LOGIC_VECTOR(unsigned(DATA(DATA_WIDTH-1 downto 0) xor "11111111111111111111111111111111") + 1);
+                    next_numer <= STD_LOGIC_VECTOR(unsigned(DATA(DATA_WIDTH-1 downto 0) xor "11111111111111111111111111111111") + 1);
                 else
-                    numer <= DATA(DATA_WIDTH-1 downto 0);
+                    next_numer <= DATA(DATA_WIDTH-1 downto 0);
                 end if;
+                next_addr <= stack_addr;
                 next_state <= ONE;
             when ONE =>
                 CHAR_OUT <= (others => '0');
                 WR_EN <= '0';
                 RD_REQ <= '0';
+                next_addr <= stack_addr;
+                next_numer <= numer;
                 next_state <= TWO;
             when TWO =>
                 CHAR_OUT <= (others => '0');
                 WR_EN <= '0';
                 RD_REQ <= '0';
+                next_addr <= stack_addr;
+                next_numer <= numer;
                 next_state <= THREE;
             when THREE =>
                 CHAR_OUT <= (others => '0');
                 WR_EN <= '0';
                 RD_REQ <= '0';
+                next_addr <= stack_addr;
+                next_numer <= numer;
                 next_state <= FOUR;
             when FOUR =>
                 CHAR_OUT <= (others => '0');
                 WR_EN <= '0';
                 RD_REQ <= '0';
-                next_state <= FIVE;
-            when FIVE =>
+                next_addr <= stack_addr;
+                next_numer <= numer;
+                next_state <= ADD_STACK;
+            when ADD_STACK =>
                 CHAR_OUT <= (others => '0');
                 WR_EN <= '0';
                 RD_REQ <= '0';
                 if unsigned(quot) = 0 then
                     next_state <= ADD_SIGN;
+                    next_numer <= numer;
                 else
                     next_state <= ONE;
-                    numer <= quot;
+                    next_numer <= quot;
                 end if;
-                idx := idx + 1;
+                next_addr <= STD_LOGIC_VECTOR(unsigned(stack_addr) + 1);
                 stack_val <= remain;
-                stack_addr <= STD_LOGIC_VECTOR(to_unsigned(idx, stack_addr'length));
                 stack_wr <= '1';
             when ADD_SIGN => 
                 if DATA(32) = '1' and DATA(31) = '1' then
@@ -139,19 +158,30 @@ begin
                     WR_EN <= '0';
                     RD_REQ <= '0';
                 end if;
+                next_addr <= stack_addr;
+                next_numer <= numer;
                 next_state <= POP;
             when POP =>
-                idx := idx - 1;
                 CHAR_OUT <= (others => '0');
                 WR_EN <= '0';
                 RD_REQ <= '0';
-                stack_addr <= STD_LOGIC_VECTOR(to_unsigned(idx, stack_addr'length));
+                next_numer <= numer;
+                next_addr <= STD_LOGIC_VECTOR(unsigned(stack_addr) - 1);
+                next_state <= LOAD;
+            when LOAD =>
+                CHAR_OUT <= (others => '0');
+                WR_EN <= '0';
+                RD_REQ <= '0';
+                next_numer <= numer;
+                next_addr <= stack_addr;
                 next_state <= WR;
             when WR =>
-                CHAR_OUT <= stack_out;
+                CHAR_OUT <= STD_LOGIC_VECTOR(unsigned(stack_out) + 48);
                 WR_EN <= '1';
                 RD_REQ <= '0';
-                if idx <= 0 then
+                next_addr <= stack_addr;
+                next_numer <= numer;
+                if unsigned(stack_addr) <= 0 then
                     next_state <= IDLE;
                 else
                     next_state <= POP;
@@ -160,6 +190,8 @@ begin
                 CHAR_OUT <= (others => '0');
                 RD_REQ <= '0';
                 WR_EN <= '0';
+                next_addr <= (others => '0');
+                next_numer <= (others => '0');
                 next_state <= IDLE;
         end case;
     end process;
@@ -190,6 +222,8 @@ begin
             state <= IDLE;
         elsif rising_edge(CLK) then
             state <= next_state;
+            numer <= next_numer;
+            stack_addr <= next_addr;
         end if;
     end process;
 end architecture behavior;
